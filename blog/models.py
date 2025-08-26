618ab2794151d701033e8aa81a3cdabb9c93202f
Raghav <raghav.patidar.42@gmail.com>
2025-08-26 18:04:00 +0530

    Add Category, Post and Comment models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,68 @@
+from django.conf import settings
 from django.db import models
+from django.db.models.functions import Lower
+from django.utils.text import slugify
 
-# Create your models here.
+
+class Category(models.Model):
+
+    title = models.CharField(max_length=50)
+    slug = models.SlugField(unique=True, null=True, blank=True)
+
+    class Meta:
+        ordering = (Lower('title'),)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        return super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"Category: {self.title}"
+
+
+class Post(models.Model):
+
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True, null=True, blank=True)
+    content = models.TextField()
+    image_url = models.ImageField(upload_to='images/')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('-created_at',)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        return super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"Post by Author: {self.user.username}"
+
+
+class Comment(models.Model):
+
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
+    comment_text = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('-created_at',)
+
+    def __str__(self):
+        return f"Comment on Post {self.post.title}."
