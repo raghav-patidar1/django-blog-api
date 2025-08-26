e981f471a426d75d8cdf7cdf1d87f179565d089f
Raghav <raghav.patidar.42@gmail.com>
2025-08-26 13:40:31 +0530

    Add signup and self-service user API views

--- users/views.py
+++ users/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework.generics import CreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from .serializers import UserAccountSerializer
+
+
+class UserCreateAPIView(CreateAPIView):
+    """
+    API view to signup.
+    """
+    serializer_class = UserAccountSerializer
+
+
+class UserMeAPIView(RetrieveUpdateDestroyAPIView):
+    """
+    API view to retrieve, update, or delete a self account.
+    """
+    serializer_class = UserAccountSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
