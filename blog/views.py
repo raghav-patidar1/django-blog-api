b324adea3cb9ec62578f71413dbad4089f825f6d
Raghav <raghav.patidar.42@gmail.com>
2025-08-27 18:24:01 +0530

    Create viewsets for Category and Post

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from .models import Category, Post
+from .permissions import IsAdminOrReadOnly, IsOwnerOrAdminDeleteOnly
+from .serializers import CategorySerializer, PostSerializer
+
+
+class CategoryViewSet(ModelViewSet):
+    """
+    ViewSet for creating, listing, updating, and deleting blog categories.
+    """
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [IsAdminOrReadOnly]
+
+
+class PostViewSet(ModelViewSet):
+    """
+    ViewSet for creating, listing, updating, and deleting blog posts.
+    """
+    queryset = Post.objects.select_related('user', 'category')
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrAdminDeleteOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+    def perform_update(self, serializer):
+        serializer.save(user=self.request.user)
