6a8d36ad98792a08d39e6beeabd55129a48e0481
Raghav <raghav.patidar.42@gmail.com>
2025-08-28 10:08:21 +0530

    Setup endpoints for Comment API views

--- blog/urls.py
+++ blog/urls.py
@@ -1,3 +1,4 @@
+from django.urls import path
 from rest_framework import routers
 
 from . import views
@@ -6,3 +7,16 @@ router = routers.SimpleRouter()
 router.register(r'categories', views.CategoryViewSet, basename='category')
 router.register(r'posts', views.PostViewSet, basename='post')
 urlpatterns = router.urls
+
+urlpatterns += [
+    path(
+        'posts/<int:post_id>/comments/',
+        views.CommentListCreateAPIView.as_view(),
+        name='comment-list-create'
+    ),
+    path(
+        'posts/<int:post_id>/comments/<int:comment_id>',
+        views.CommentDetailAPIView.as_view(),
+        name='comment-detail'
+    ),
+]
